0df4f4ce1473a4df78cfb9d6b396fab63500be6d
Max Marriott-Clarke <maxmarriottclarke@me.com>
2026-04-14 08:03:42 +0200

    Added int size promotions

---
+++ playground/cpp_avx2/int_size_promotion/int_size_promotion.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "YmmVal.h"
+
+extern void ZeroExtU8_U16_Iavx2(YmmVal c[2], YmmVal* a);
+extern void ZeroExtU8_U32_Iavx2(YmmVal c[4], YmmVal* a);
+extern void SignExtI16_I32_Iavx2(YmmVal c[2], YmmVal* a);
+extern void SignExtI16_I64_Iavx2(YmmVal c[4], YmmVal* a);
